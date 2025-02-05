c20c2feb627f19c548c66fa803b0b701f61d4c92
azuda <aaronzhang587@gmail.com>
2025-02-05 11:28:09 -0700

    anim bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,34 @@
 from manim import *
 
 class Sort(Scene):
+  def create_textbox(self, string):
+    result = VGroup()
+    textbox = Square(
+      fill_opacity=0,
+      stroke_color="#ffffff"
+    )
+    text = Text(string, color="#ffffff").move_to(textbox.get_center())
+    result.add(textbox, text)
+    return result
+
   def construct(self):
     # scene setup
+    boxes = VGroup()
+    for i, node in enumerate(unsorted):
+      box = self.create_textbox(str(node))
+      box.shift(RIGHT * i * 0.8)
+      box.scale(0.4)
+      boxes.add(box)
+    boxes.move_to(ORIGIN)
+    self.add(boxes)
+    self.play(Write(boxes), run_time=1)
+
+    # create cursors at start of input list
+    left = Square(color=BLUE, fill_opacity=0.5).scale(0.4)
+    right = Square(color=PINK, fill_opacity=0.5).scale(0.4)
+    left.move_to(boxes[0])
+    right.move_to(boxes[1])
+    self.play(Write(left), Write(right), run_time=1)
 
     # algo
     def bubble_sort(input_list, n=None):
@@ -14,14 +40,34 @@ class Sort(Scene):
         return input_list
 
       # recursive case
-      for i in range(n-1):
-        if input_list[i] > input_list[i+1]:
-          input_list[i], input_list[i+1] = input_list[i+1], input_list[i]
+      for j in range(n-1):
+        # animate cursor movement
+        self.play(
+          left.animate.move_to(boxes[j]),
+          right.animate.move_to(boxes[j+1]),
+          run_time=0.5
+        )
+
+        if input_list[j] > input_list[j+1]:
+          input_list[j], input_list[j+1] = input_list[j+1], input_list[j]
+
+          # animate swapping list elements
+          self.play(
+            Swap(boxes[j], boxes[j+1], run_time=0.5, path_arc=120*DEGREES),
+            Swap(left, right, run_time=0.5, path_arc=120*DEGREES)
+          )
+          boxes[j], boxes[j+1] = boxes[j+1], boxes[j]
 
       return bubble_sort(input_list, n-1)
 
-
+    # call sort algo
     bubble_sort(unsorted)
+
+    # remove cursors from scene
+    self.play(FadeOut(left), FadeOut(right))
+    self.remove(left, right)
+
+    # done
     self.wait(1)
 
 
@@ -29,4 +75,4 @@ class Sort(Scene):
 
 
 
-unsorted = [111, 23, 1, 15, 9, 3, 27, 19, 5, 11, 7, 21, 13, 29, 17, 25]
+unsorted = [111, 23, 1, 15, 9, 3, 27, 19, 5, 11, 7, 21, 13, 29, 17, 25, 99]
