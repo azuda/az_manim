b21f0f5184e34842dca8af9b5212f50bfd9b4f94
azuda <aaronzhang587@gmail.com>
2025-02-10 14:31:50 -0700

    split anim borked

--- merge_sort.py
+++ merge_sort.py
@@ -20,46 +20,54 @@ class Sort(Scene):
       box.scale(0.4)
       boxes.add(box)
     boxes.move_to(ORIGIN)
-    self.add(boxes)
     self.play(Write(boxes), run_time=1)
+    sorted_boxes = VGroup()
+    sorted_boxes.add(Square(color=GREEN, fill_opacity=0.2).scale(0.4))
+    sorted_boxes.next_to(boxes, DOWN * 4)
+    self.add(sorted_boxes)
 
     # sort algo + anim
-    def merge_sort(arr):
+    def merge_sort(arr, i_boxes=boxes):
       if len(arr) > 1:
         mid = len(arr) // 2
         left = arr[:mid]
         right = arr[mid:]
 
-        # left_boxes = boxes[:mid]
-        # right_boxes = boxes[mid:]
-        # self.play(
-        #   left_boxes.animate.shift(LEFT * 0.1),
-        #   right_boxes.animate.shift(RIGHT * 0.1),
-        #   run_time=0.5
-        # )
+        left_boxes = i_boxes[:mid]
+        right_boxes = i_boxes[mid:]
+        self.play(
+          left_boxes.animate.shift(LEFT * 0.1),
+          right_boxes.animate.shift(RIGHT * 0.1),
+          run_time=0.5
+        )
 
-        merge_sort(left)
-        merge_sort(right)
+        merge_sort(left, left_boxes)
+        merge_sort(right, right_boxes)
 
         x = y = z = 0
+        self.add(sorted_boxes)
         while x < len(left) and y < len(right):
           if left[x] < right[y]: # left is lower
             arr[z] = left[x]
+            sorted_boxes.add(left_boxes[x])
             x += 1
           else: # right is lower
             arr[z] = right[y]
+            sorted_boxes.add(right_boxes[y])
             y += 1
-          z += 1
+          z += 1 # increment to next spot in sorted list
 
         # cleanup left remainder
         while x < len(left):
           arr[z] = left[x]
+          sorted_boxes.add(left_boxes[x])
           x += 1
           z += 1
 
         # cleanup right remainder
         while y < len(right):
           arr[z] = right[y]
+          sorted_boxes.add(right_boxes[y])
           y += 1
           z += 1
 
@@ -67,6 +75,11 @@ class Sort(Scene):
 
     # call merge sort algo
     merge_sort(unsorted)
+
+    for a, split_box in enumerate(sorted_boxes):
+      split_box.move_to(sorted_boxes.get_center())
+      split_box.shift(RIGHT * a * 2)
+    sorted_boxes.move_to(ORIGIN)
 
     # done
     self.wait(1)
